0cf5d9f5e764c54a7feec4aa9bf987f2e0a14687
FingiD <dyar.jankir@alumni.fh-aachen.de>
2025-06-03 21:33:18 +0200

    test: add unit test for 'check_all_keywords_in_image'
    - Testing finding all keywords in an valid image (`valid_image`)
    - Testing that not all keywords were found because they were not present in partial_image (`partial_image`)

---
+++ tests/test_check_all_keywords_in_image.py
@@ -0,0 +1,37 @@
+import cv2
+from pathlib import Path
+import numpy as np
+from slideDetection import check_all_keywords_in_image
+
+
+def pytest_generate_tests(metafunc):
+
+    TEST_DATA_PATH: Path = (
+        Path(__file__).parent / "test_data" / "test_check_all_keywords_in_image"
+    )
+
+    if "valid_image" in metafunc.fixturenames:
+        valid_images: list[Path] = list((TEST_DATA_PATH / "valid").iterdir())
+        metafunc.parametrize(
+            "valid_image", valid_images, ids=[p.name for p in valid_images]
+        )
+
+    if "partial_image" in metafunc.fixturenames:
+        partial_images: list[Path] = list((TEST_DATA_PATH / "partial").iterdir())
+        metafunc.parametrize(
+            "partial_image", partial_images, ids=[p.name for p in partial_images]
+        )
+
+
+class TestKeywordDetection:
+    def test_valid_images(self, valid_image):
+        img: np.ndarray = cv2.imread(str(valid_image))
+        assert img is not None, f"Could not load image {valid_image}"
+        assert check_all_keywords_in_image(img) is True, f"Failed on {valid_image.name}"
+
+    def test_partial_images(self, partial_image):
+        img: np.ndarray = cv2.imread(str(partial_image))
+        assert img is not None, f"Could not load image {partial_image}"
+        assert (
+            check_all_keywords_in_image(img) is False
+        ), f"Failed on {partial_image.name}"
