7353d53c86f4be0866f6e8c29f8e2b7d31434947
FingiD <dyar.jankir@alumni.fh-aachen.de>
2025-06-12 18:37:56 +0200

    feat: add function to convert PDF slides to OpenCV Images (Closes #7)
    This function extracts each slide from a PDF, converts it into a Pillow image,
    and transforms it into an OpenCV-compatible format (`np.ndarray` in BGR).
    The extracted slides are returned as a list for further processing.

--- slideDetection.py
+++ slideDetection.py
@@ -1,15 +1,44 @@
 import cv2
 import numpy as np
 import pyautogui
+from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 import pytesseract  # type: ignore
 import re
 from typing import Union
+import pymupdf  # type: ignore
 
 DEBUG_MODE = False
 
 # Threshold for detecting slide changes
 STRUCTURAL_SIMILARITY_THRESHOLD = 0.85
+
+
+def convert_pdf_slides_to_images(pdf_path: str) -> list[np.ndarray]:
+    """
+    Extracts each single slide from a PDF and converts them into OpenCV images.
+
+    This function loads each slide (page) of the given PDF, gets the pixel map of it,
+    converts it into a Pillow image, and transforms it into an OpenCV-compatible format (BGR `np.ndarray`).
+    The extracted slides are returned as a list of OpenCV images for further processing.
+
+    Args:
+        pdf_path: Path to the PDF file containing slides.
+
+    Returns:
+        A list of OpenCV images, each representing a slide from the PDF, in order.
+    """
+    pdf_document: pymupdf.Document = pymupdf.open(pdf_path)
+    pdf_slide_images_cv2: list[np.ndarray] = []
+
+    for i in range(pdf_document.page_count):
+        current_page_pixmap: pymupdf.Pixmap = pdf_document.get_page_pixmap(i, dpi=200)
+        pillow_image: Image.Image = current_page_pixmap.pil_image()
+        open_cv_image: np.ndarray = cv2.cvtColor(np.array(pillow_image), cv2.COLOR_RGB2BGR)
+        pdf_slide_images_cv2.append(open_cv_image)
+
+    pdf_document.close()
+    return pdf_slide_images_cv2
 
 
 def show_image_resized(image: np.ndarray, window_name: str = "default", scale_factor: float = 0.8) -> None:
